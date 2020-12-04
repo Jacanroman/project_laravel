1bfd1020c6ac6e9e5d64128a22beb0601700bd9d
Jacanroman <jacanroman@gmail.com>
2020-12-04 18:00:48 +0000

    testing the ORM

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//para poder utilizar la clase incluimos el model.(en esta caso Image)
+use App\Image;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    //conseguir todas las images de la tabla images
+    $images = Image::all();
+    foreach($images as $image){
+        echo $image->image_path."<br/>";
+        echo $image->description."<br/>";
+        
+        echo $image->user->name.' '.$image->user->surname.'<br/>';
+
+        //si los comentarios son 1 o mas los muestra si no no
+        if(count($image->comments) >=1){
+            echo "<br/>";
+            echo '<strong>COMENTARIOS</strong><br/>';
+            foreach ($image->comments as $comment) {
+                echo $comment->user->name.' '.$comment->user->surname.':';
+                echo $comment->content . "<br/>";
+            }
+        }
+
+        echo 'LIKES: '.count($image->likes);
+        echo "<hr/>";   
+    }
+    
+    die();
+    //return view('welcome');
 });
